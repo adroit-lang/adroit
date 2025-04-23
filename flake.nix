8066255b7e7cdafddc34960f617dd8e5b24f1235
Sam Estep <sam@samestep.com>
2025-04-23 14:24:20 -0400

    Add a dev shell in a Nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A programming language";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              bun
+              cargo
+              nixfmt-rfc-style
+            ];
+          };
+      }
+    );
+}
